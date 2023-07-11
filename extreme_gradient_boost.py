4b452d9b702874d287fd03dbf5dc7fefd9a52d68
Martin Zahren <70772732+zahrenmartin@users.noreply.github.com>
2023-07-11 13:34:55 +0200

    added randomForst and xgboost

--- extreme_gradient_boost.py
+++ extreme_gradient_boost.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 import xgboost as xgb
 
+# Generate synthetic dataset
+np.random.seed(0)
 
 # e.g., [i, x, z, month]
 X = np.array([[5.48, 2.3, 1.7, 8],
@@ -24,13 +24,13 @@ X = np.array([[5.48, 2.3, 1.7, 8],
               [5.48, 2.3, 1.7, 8],
               [2.15, 3.1, 2.6, 10],
               [6.02, 1.5, 4.2, 3],
-              [7.02,1.5, 4.2, 4]])
+              [7.02, 1.5, 4.2, 4]])
 
 y = [12.74032243,
-     14.70394453, 
-     13.0340055,   
-     9.21716328, 
-     16.17134531, 
+     14.70394453,
+     13.0340055,
+     9.21716328,
+     16.17134531,
      11.07798964,
      11.9263585,
      14.15243011,
@@ -38,13 +38,13 @@ y = [12.74032243,
      12.46586602,
      11.12031365,
      15.75806083,
-     12.81630525, 
-     11.0979451,  
-     14.74765055, 
-     6.38894185, 
-     12.47034915, 
+     12.81630525,
+     11.0979451,
+     14.74765055,
+     6.38894185,
+     12.47034915,
      4.09862906,
-     12.36833522, 
+     12.36833522,
      13.20117178]
 
 print("Training parameters:")
@@ -53,18 +53,8 @@ print("Training target variables:")
 print(y)
 
 # Create and train the model
-
-# Linear Regression
-linRegModel = LinearRegression()
-linRegModel.fit(X, y)
-
-# RandomForest
-randomForestModel = RandomForestRegressor()
-randomForestModel.fit(X, y)
-
-# XGBoost
-xgBoostModel = xgb.XGBRegressor()
-xgBoostModel.fit(X, y)
+model = xgb.XGBRegressor()
+model.fit(X, y)
 
 # Make predictions on new data
 # next 12 month for example
@@ -81,26 +71,10 @@ new_data = np.array([[5.3, 1.1, 2.4, 3.7],
                      [4.5, 2.7, 4.9, 3.8],
                      [6.5, 3.1, 1.7, 5.3]])
 
-linRegPredictions = linRegModel.predict(new_data)
-randomForestPredictions = randomForestModel.predict(new_data)
-xgBoostPredictions = xgBoostModel.predict(new_data)
+predictions = model.predict(new_data)
 
 # Print the coefficients and predictions
-print("Linear Regression Coefficients:", linRegModel.coef_)
-print("Linear Regression Intercept:", linRegModel.intercept_)
-print("Linear Regression Predictions:")
-for prediction in linRegPredictions:
-    print(prediction)
-
-# Print the coefficients and predictions
-print("Feature Importances:", randomForestModel.feature_importances_)
-print("RandomForest Predictions:")
-for prediction in randomForestPredictions:
-    print(prediction)
-
-
-# Print the coefficients and predictions
-print("Feature Importances:", xgBoostModel.feature_importances_)
-print("XGBoost Predictions:")
-for prediction in xgBoostPredictions:
+print("Feature Importances:", model.feature_importances_)
+print("Predictions:")
+for prediction in predictions:
     print(prediction)
--- lin_regression.py
+++ lin_regression.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-import xgboost as xgb
 
+# Generate synthetic dataset
+np.random.seed(0)
 
 # e.g., [i, x, z, month]
 X = np.array([[5.48, 2.3, 1.7, 8],
@@ -53,18 +53,8 @@ print("Training target variables:")
 print(y)
 
 # Create and train the model
-
-# Linear Regression
-linRegModel = LinearRegression()
-linRegModel.fit(X, y)
-
-# RandomForest
-randomForestModel = RandomForestRegressor()
-randomForestModel.fit(X, y)
-
-# XGBoost
-xgBoostModel = xgb.XGBRegressor()
-xgBoostModel.fit(X, y)
+model = LinearRegression()
+model.fit(X, y)
 
 # Make predictions on new data
 # next 12 month for example
@@ -81,26 +71,11 @@ new_data = np.array([[5.3, 1.1, 2.4, 3.7],
                      [4.5, 2.7, 4.9, 3.8],
                      [6.5, 3.1, 1.7, 5.3]])
 
-linRegPredictions = linRegModel.predict(new_data)
-randomForestPredictions = randomForestModel.predict(new_data)
-xgBoostPredictions = xgBoostModel.predict(new_data)
+predictions = model.predict(new_data)
 
 # Print the coefficients and predictions
-print("Linear Regression Coefficients:", linRegModel.coef_)
-print("Linear Regression Intercept:", linRegModel.intercept_)
-print("Linear Regression Predictions:")
-for prediction in linRegPredictions:
-    print(prediction)
-
-# Print the coefficients and predictions
-print("Feature Importances:", randomForestModel.feature_importances_)
-print("RandomForest Predictions:")
-for prediction in randomForestPredictions:
-    print(prediction)
-
-
-# Print the coefficients and predictions
-print("Feature Importances:", xgBoostModel.feature_importances_)
-print("XGBoost Predictions:")
-for prediction in xgBoostPredictions:
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
+print("Predictions:")
+for prediction in predictions:
     print(prediction)
