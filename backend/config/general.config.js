5c95bb1d238c8408eb7c16cd61e70160c0b03fd1
ArslanThobani <thobaniarslan@gmail.com>
2023-07-24 05:44:09 +0200

    Update general.config.js

--- backend/config/general.config.js
+++ backend/config/general.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    clientURL: 'http://44.201.90.106', 
-    clientURLhttps: 'http://44.201.90.106',
-    localClientURL: 'http://44.201.90.106', 
+    clientURL: 'http://44.201.230.15', 
+    clientURLhttps: 'http://44.201.230.15',
+    localClientURL: 'http://44.201.230.15', 
     apiURL: 'http://localhost:8080'
    };  
